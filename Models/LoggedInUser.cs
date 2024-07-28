7507491d33cea1b48262f7c4e4db52382974b6b2
A-00299373 <156479142+A-00299373@users.noreply.github.com>
2024-07-27 22:33:27 -0400

    Update LoggedInUser.cs

--- Models/LoggedInUser.cs
+++ Models/LoggedInUser.cs
@@ -8,6 +8,8 @@ namespace TripTracker.Models
     /// name (Name). It provides basic user information needed for session
     /// management and personalization features within the app.
     /// </summary>
+/// this is trial
+
     public readonly record struct LoggedInUser(int Id, string Name)
     {
         public string ToJson() => JsonSerializer.Serialize(this);
