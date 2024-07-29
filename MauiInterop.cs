1ad12f6e55f26b4fdfb73df8359fedb55d089d00
Aanya2 <manav01patel@gmail.com>
2024-07-29 13:58:19 -0400

    Added error and success mesaage while signin in.

--- MauiInterop.cs
+++ MauiInterop.cs
@@ -16,5 +16,11 @@ namespace TripTracker
 
         public void ShowLoader() => _appViewModel.ToggleIsBusy(true);
         public void HideLoader() => _appViewModel.ToggleIsBusy(false);
+
+        public async Task ShowErrorAlertAsync(string message, string? title = "Error") =>
+            await App.Current.MainPage.DisplayAlert(title, message, "Ok");
+
+        public async Task ShowSuccessAlertAsync(string message, string? title = "Success") =>
+            await App.Current.MainPage.DisplayAlert(title, message, "Ok");
     }
 }
