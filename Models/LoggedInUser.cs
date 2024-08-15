c2ef03c7cfb69bf1ec382e8397ba6a2495180cf9
Osamp <A00280655@mycambrian.ca>
2024-08-14 23:31:54 -0400

    Added ManageExpenseCategories.razor for managing expense categories and updated related components. Integrated category management in SettingsPage.razor, added name and password change functionalities in AuthService.cs, and implemented expense category saving in TripsService.cs. Enhanced MauiInterop.cs with prompt and launcher methods. Improved UI styling in SettingsPage.razor.

--- Models/LoggedInUser.cs
+++ Models/LoggedInUser.cs
@@ -8,10 +8,14 @@ namespace TripTracker.Models
     /// name (Name). It provides basic user information needed for session
     /// management and personalization features within the app.
     /// </summary>
-/// this is trial
 
     public readonly record struct LoggedInUser(int Id, string Name)
     {
         public string ToJson() => JsonSerializer.Serialize(this);
+
+        public static LoggedInUser LoadFromJson(string? json) =>
+            string.IsNullOrWhiteSpace(json)
+            ? default
+            : JsonSerializer.Deserialize<LoggedInUser>(json);
     }
 }
